375525d21f32be543b8bbc1cd0a893304ce3ecbc
YaelDev <jorge.yael333@gmail.com>
2025-11-26 17:02:34 -0600

    Iniciando controlador y testing

--- src/main/java/com/taco/cloud/taco_cloud/HomeController.java
+++ src/main/java/com/taco/cloud/taco_cloud/HomeController.java
@@ -1,10 +1,13 @@
 package com.taco.cloud.taco_cloud;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String home(){
         return "home";
     }
---
+++ src/test/java/com/taco/cloud/taco_cloud/controllers/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package com.taco.cloud.taco_cloud.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.containsString;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import com.taco.cloud.taco_cloud.HomeController;
+
+
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void testHome() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(content().string(
+                containsString("Welcome to...")
+            ));
+    }
+
+}
